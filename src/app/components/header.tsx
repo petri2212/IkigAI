3e0d400eafbb6d1ed3d06e052af4fcc36153abea
zodiacapricorn <a.radu@studenti.unibg.it>
2025-08-04 13:15:07 +0200

    updated header

--- src/app/components/header.tsx
+++ src/app/components/header.tsx
@@ -4,12 +4,16 @@ import Image from 'next/image';
 import Link from 'next/link';
 import { useRouter } from 'next/navigation';
 import { usePathname } from 'next/navigation';
+import { useSignOut } from 'react-firebase-hooks/auth';
+import { auth } from '@/infrastructure/firebase/config';
 
 export default function Header() {
   const [atTop, setAtTop] = useState(true);
   const router = useRouter();
   const pathname = usePathname();
-  const showLoginButton = pathname !== '/sign-in' && pathname !== '/sign-up';
+  const showLoginButton = pathname !== '/sign-in' && pathname !== '/sign-up' && pathname !== '/sign-up';
+  const isProtectedPage = pathname.startsWith('/protected');
+  const [signOut, signOutLoading, signOutError] = useSignOut(auth);
 
   useEffect(() => {
     const handleScroll = () => {
@@ -20,8 +24,13 @@ export default function Header() {
     return () => window.removeEventListener('scroll', handleScroll);
   }, []);
 
+  const handleLogout = async () => {
+    const success = await signOut();
+    if (success) router.push('/');
+  };
+
   return (
-    <header className={`fixed top-0 left-0 w-full z-50 ${showLoginButton ? 'bg-background/90' : 'bg-transparent'}`}>
+    <header className={`fixed top-0 left-0 w-full z-50 ${showLoginButton && !isProtectedPage? 'bg-background/90' : 'bg-transparent'}`}>
       <div
         className={`flex items-center px-6 py-4 max-w-7xl mx-auto ${showLoginButton ? 'justify-between' : 'justify-center'
           }`}
@@ -36,7 +45,7 @@ export default function Header() {
           />
         </Link>
 
-        {showLoginButton && (
+        {showLoginButton && !isProtectedPage && (
           <button
             onClick={() => router.push('/sign-in')}
             className="bg-gray-600 hover:bg-gray-700 transition text-white font-semibold py-2 px-6 rounded-xl cursor-pointer"
@@ -44,6 +53,21 @@ export default function Header() {
             Sign In
           </button>
         )}
+
+        {isProtectedPage && (
+          <button
+            onClick={handleLogout}
+            disabled={signOutLoading}
+            className="bg-red-700/90 hover:bg-red-800 text-white font-bold py-2 px-4 rounded-xl transition"
+          >
+            {signOutLoading ? 'Logout...' : 'Logout'}
+          </button>
+        )}
+
+        {signOutError && (
+          <p className="text-red-400 mt-4">Errore durante il logout: {signOutError.message}</p>
+        )}
+
       </div>
     </header>
 
