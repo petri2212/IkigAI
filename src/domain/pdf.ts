49ce0ca3a6a60db5608beca2aadff36431a33ad9
Andrei <a.petrisor@studenti.unibg.it>
2025-08-06 12:01:50 +0200

    Update comments for clarity

--- src/domain/pdf.ts
+++ src/domain/pdf.ts
@@ -1,5 +1,5 @@
 export interface PdfDocument {
-  _id: string;          // la tua stringa usata come _id
+  _id: string;          // string used like _id
   tipo: string;
   file: Buffer;
   uploadedAt: Date;
