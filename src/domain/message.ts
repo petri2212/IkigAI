9d33c1f01068a7f5e91e391bd83d6c2d858ce2d1
Andrei <a.petrisor@studenti.unibg.it>
2025-08-11 14:09:07 +0200

    aplified message.ts

--- src/domain/message.ts
+++ src/domain/message.ts
@@ -8,3 +8,17 @@ export interface Message {
     timestamp?: Date;
   }[];
 }
+
+export interface SessionDoc {
+        _id?: import("mongodb").ObjectId;
+        id: string;
+        number_session: string;
+        q_and_a: QA[];
+        createdAt: Date;
+      }
+
+export interface QA {
+        question: string;
+        answer: string;
+        timestamp: Date;
+      }
